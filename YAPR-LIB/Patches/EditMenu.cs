28a9ae596a0c6d21c68ed39a6b0ace30dafb8ebf
UltiNaruto <zenaruto@ymail.com>
2024-08-31 21:22:46 +0200

    Fixed code detection due to UTMT simplifying the decompiled code

--- YAPR-LIB/Patches/EditMenu.cs
+++ YAPR-LIB/Patches/EditMenu.cs
@@ -12,23 +12,23 @@ namespace YAPR_LIB.Patches
 
             // patching the menu so you get only the planet you're playing
             if (planetName == "zebeth")
-                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(0, xx, (yy + (sep * 1)), 65, 9, \"PLANET ZEBETH\", \"\", 0, 1, -1, -1, -1, 1, -1)\n", "        scr_TS_Build_Option(0, xx, (yy + (sep * 1)), 65, 9, \"RANDOVANIA\", \"\", 0, 1, -1, -1, -1, 4, -1)\n");
+                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(0, xx, (yy + sep * 1), 65, 9, \"PLANET ZEBETH\", \"\", 0, 1, -1, -1, -1, 1, -1)\n", "        scr_TS_Build_Option(0, xx, (yy + sep * 1), 65, 9, \"RANDOVANIA\", \"\", 0, 1, -1, -1, -1, 4, -1)\n");
             else
-                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(0, xx, (yy + (sep * 1)), 65, 9, \"PLANET ZEBETH\", \"\", 0, 1, -1, -1, -1, 1, -1)\n", "        scr_TS_Build_Option(1, xx, (yy + (sep * 99)), 65, 9, \"\", \"\", 1, 1, -1, -1, -1, -1, -1)\n");
+                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(0, xx, (yy + sep * 1), 65, 9, \"PLANET ZEBETH\", \"\", 0, 1, -1, -1, -1, 1, -1)\n", "");
             if (planetName == "novus")
-                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(1, xx, (yy + (sep * 2)), 65, 9, \"PLANET NOVUS\", \"\", 1, 1, -1, 0, -1, 2, -1)\n", "        scr_TS_Build_Option(0, xx, (yy + (sep * 1)), 65, 9, \"RANDOVANIA\", \"\", 1, 1, -1, -1, -1, 4, -1)\n");
+                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(1, xx, (yy + sep * 2), 65, 9, \"PLANET NOVUS\", \"\", 1, 1, -1, 0, -1, 2, -1)\n", "        scr_TS_Build_Option(0, xx, (yy + sep * 1), 65, 9, \"RANDOVANIA\", \"\", 1, 1, -1, -1, -1, 4, -1)\n");
             else
-                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(1, xx, (yy + (sep * 2)), 65, 9, \"PLANET NOVUS\", \"\", 1, 1, -1, 0, -1, 2, -1)\n", "        scr_TS_Build_Option(1, xx, (yy + (sep * 99)), 65, 9, \"\", \"\", 1, 1, -1, -1, -1, -1, -1)\n");
+                ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(1, xx, (yy + sep * 2), 65, 9, \"PLANET NOVUS\", \"\", 1, 1, -1, 0, -1, 2, -1)\n", "");
             // if mobile == 0
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(2, xx, (yy + (sep * 3)), 65, 9, \"PLANET ENIGMA\", \"\", 2, 1, -1, 1, -1, 3, -1)\n", "        scr_TS_Build_Option(2, xx, (yy + (sep * 99)), 65, 9, \"\", \"\", 2, 1, -1, -1, -1, -1, -1)");
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(3, xx, (yy + (sep * 4)), 65, 9, \"CONNECT\", \"\", 3, 1, -1, 2, -1, 4, -1)\n", "        scr_TS_Build_Option(3, xx, (yy + (sep * 99)), 65, 9, \"\", \"\", 3, 1, -1, -1, -1, -1, -1)");
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(4, xx, (yy + (sep * 5)), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 3, -1, 6, -1)\n", "        scr_TS_Build_Option(4, xx, (yy + (sep * 2)), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 0, -1, 7, -1)\n");
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(6, xx, (yy + (sep * 6)), 65, 9, \"CREDITS\", \"\", 5, 1, -1, 4, -1, 7, -1)\n", "        scr_TS_Build_Option(6, xx, (yy + (sep * 99)), 65, 9, \"CREDITS\", \"\", 5, 1, -1, -1, -1, -1, -1)\n");
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(7, xx, (yy + (sep * 7)), 65, 9, \"QUIT\", \"\", 6, 1, -1, 6, -1, -1, -1)\n", "        scr_TS_Build_Option(7, xx, (yy + (sep * 3)), 65, 9, \"QUIT\", \"\", 6, 1, -1, 4, -1, -1, -1)\n");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(2, xx, (yy + sep * 3), 65, 9, \"PLANET ENIGMA\", \"\", 2, 1, -1, 1, -1, 3, -1)\n", "");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(3, xx, (yy + sep * 4), 65, 9, \"CONNECT\", \"\", 3, 1, -1, 2, -1, 4, -1)\n", "");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(4, xx, (yy + sep * 5), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 3, -1, 6, -1)\n", "        scr_TS_Build_Option(4, xx, (yy + sep * 2), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 0, -1, 7, -1)\n");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(6, xx, (yy + sep * 6), 65, 9, \"CREDITS\", \"\", 5, 1, -1, 4, -1, 7, -1)\n", "");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(7, xx, (yy + sep * 7), 65, 9, \"QUIT\", \"\", 6, 1, -1, 6, -1, -1, -1)\n", "        scr_TS_Build_Option(7, xx, (yy + sep * 3), 65, 9, \"QUIT\", \"\", 6, 1, -1, 4, -1, -1, -1)\n");
             // if mobile == 1
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(2, xx, (yy + (sep * 3)), 65, 9, \"CONNECT\", \"\", 3, 1, -1, 1, -1, 3, -1)\n", "        scr_TS_Build_Option(2, xx, (yy + (sep * 99)), 65, 9, \"\", \"\", 3, 1, -1, -1, -1, -1, -1)\n");
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(3, xx, (yy + (sep * 4)), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 3, -1, 4, -1)\n", "        scr_TS_Build_Option(4, xx, (yy + (sep * 2)), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 0, -1, -1, -1)\n");
-            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(4, xx, (yy + (sep * 6)), 65, 9, \"CREDITS\", \"\", 5, 1, -1, 3, -1, -1, -1)\n", "        scr_TS_Build_Option(6, xx, (yy + (sep * 99)), 65, 9, \"CREDITS\", \"\", 5, 1, -1, -1, -1, -1, -1)\n");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(2, xx, (yy + sep * 3), 65, 9, \"CONNECT\", \"\", 3, 1, -1, 1, -1, 3, -1)\n", "");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(3, xx, (yy + sep * 4), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 3, -1, 4, -1)\n", "        scr_TS_Build_Option(4, xx, (yy + sep * 2), 65, 9, \"OPTIONS\", \"\", 4, 1, -1, 0, -1, -1, -1)\n");
+            ts_setup_menu = ts_setup_menu.Replace("        scr_TS_Build_Option(4, xx, (yy + sep * 6), 65, 9, \"CREDITS\", \"\", 5, 1, -1, 3, -1, -1, -1)\n", "");
 
             // remove all of the options when starting game
             ts_setup_menu = ts_setup_menu.Replace("    scr_TS_Build_Option(1, xx, (yy + 50), 110, 15, \"DIFFICULTY\", \"NORMAL-HARD-\", 2, 4, -1, 0, -1, 2, global.GAME_DIFFICULTY)\n", "");
@@ -44,11 +44,11 @@ namespace YAPR_LIB.Patches
                     sep = 20
                 """.ReplaceLineEndings("\n")
             );
-            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(3, (xx - 80), (yy + (sep * 4)), 70, 14, \"WINDOW SCALE\", \"x2-x3-MAX-\", 2, 4, 6, 2, -1, 4, -1)", "scr_TS_Build_Option(3, (xx - 80), (yy + (sep * 4)), 70, 14, \"WINDOW SCALE\", \"x2-x3-MAX-\", 2, 4, 4, 2, -1, 6, -1)");
-            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(4, (xx - 80), (yy + (sep * 5.5)), 70, 14, \"ASPECT RATIO\", \"16:9-4:3-\", 3, 4, 7, 3, -1, -1, -1)", "scr_TS_Build_Option(4, (xx + 80), (yy + (sep * 4)), 70, 14, \"ASPECT RATIO\", \"16:9-4:3-\", 3, 4, -1, 5, 3, 7, -1)");
-            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(5, (xx + 80), (yy + (sep * 2.5)), 70, 14, \"PARTICLES\", \"OFF-ON-\", 4, 4, -1, 0, 2, 6, -1)", "scr_TS_Build_Option(5, (xx + 80), (yy + (sep * 2.5)), 70, 14, \"PARTICLES\", \"OFF-ON-\", 4, 4, -1, 0, 2, 4, -1)");
-            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(6, (xx + 80), (yy + (sep * 4)), 70, 14, \"BGM VOLUME\", \"\", 5, 7, -1, 5, 3, 7, -1)", "scr_TS_Build_Option(6, (xx - 80), (yy + (sep * 5.5)), 70, 14, \"BGM VOLUME\", \"\", 5, 7, 7, 3, -1, -1, -1)");
-            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(7, (xx + 80), (yy + (sep * 5.5)), 70, 14, \"SFX VOLUME\", \"\", 6, 7, -1, 6, 4, -1, -1)", "scr_TS_Build_Option(7, (xx + 80), (yy + (sep * 5.5)), 70, 14, \"SFX VOLUME\", \"\", 6, 7, -1, 4, 6, -1, -1)");
+            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(3, (xx - 80), (yy + sep * 4), 70, 14, \"WINDOW SCALE\", \"x2-x3-MAX-\", 2, 4, 6, 2, -1, 4, -1)", "scr_TS_Build_Option(3, (xx - 80), (yy + sep * 4), 70, 14, \"WINDOW SCALE\", \"x2-x3-MAX-\", 2, 4, 4, 2, -1, 6, -1)");
+            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(4, (xx - 80), (yy + sep * 5.5), 70, 14, \"ASPECT RATIO\", \"16:9-4:3-\", 3, 4, 7, 3, -1, -1, -1)", "scr_TS_Build_Option(4, (xx + 80), (yy + sep * 4), 70, 14, \"ASPECT RATIO\", \"16:9-4:3-\", 3, 4, -1, 5, 3, 7, -1)");
+            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(5, (xx + 80), (yy + sep * 2.5), 70, 14, \"PARTICLES\", \"OFF-ON-\", 4, 4, -1, 0, 2, 6, -1)", "scr_TS_Build_Option(5, (xx + 80), (yy + sep * 2.5), 70, 14, \"PARTICLES\", \"OFF-ON-\", 4, 4, -1, 0, 2, 4, -1)");
+            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(6, (xx + 80), (yy + sep * 4), 70, 14, \"BGM VOLUME\", \"\", 5, 7, -1, 5, 3, 7, -1)", "scr_TS_Build_Option(6, (xx - 80), (yy + sep * 5.5), 70, 14, \"BGM VOLUME\", \"\", 5, 7, 7, 3, -1, -1, -1)");
+            ts_setup_menu = ts_setup_menu.Replace("scr_TS_Build_Option(7, (xx + 80), (yy + sep * 5.5), 70, 14, \"SFX VOLUME\", \"\", 6, 7, -1, 6, 4, -1, -1)", "scr_TS_Build_Option(7, (xx + 80), (yy + sep * 5.5), 70, 14, \"SFX VOLUME\", \"\", 6, 7, -1, 4, 6, -1, -1)");
 
             ts_setup_menu = ts_setup_menu.Replace(
                 """
