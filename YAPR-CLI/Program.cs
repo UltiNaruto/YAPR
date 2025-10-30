5c74dcb420996207c6c1d698796177bc853d55e8
UltiNaruto <zenaruto@ymail.com>
2025-10-30 13:55:50 +0100

    Only patch data.win from YAPR-CLI

--- YAPR-CLI/Program.cs
+++ YAPR-CLI/Program.cs
@@ -26,52 +26,6 @@ public class Program
         try {
             Patcher.Main(inputPath, outputPath, jsonPath);
 
-            var randomizerConfig = JsonSerializer.Deserialize<RandomizerConfig>(File.ReadAllText(jsonPath));
-            if (randomizerConfig is null)
-                throw new Exception("Invalid config detected!");
-            if (randomizerConfig.LevelData is null)
-                throw new Exception("Invalid config detected!");
-            if (randomizerConfig.GameConfig is null)
-                throw new Exception("Invalid config detected!");
-            var inputDir = Directory.GetParent(inputPath);
-            if (inputDir is null)
-                throw new DirectoryNotFoundException("Couldn't find the parent folder of input path!");
-            var outputDir = Directory.GetParent(outputPath);
-            if (outputDir is null)
-                throw new DirectoryNotFoundException("Couldn't find the parent folder of output path!");
-
-            // Copy original files to randomized game
-            var filesToCopy = new string[] {
-                "8bitoperator_jve.ttf",
-                "audiogroup1.dat",
-                "audiogroup2.dat",
-                "DialogModule.dll",
-                "execute_shell_simple.dll",
-                "GMS-WinDev.dll",
-                "Metroid Planets v1.27g.exe",
-                "options.ini"
-            };
-                foreach (var file in filesToCopy)
-                    if (File.Exists(Path.Combine(inputDir.FullName, file)))
-                    {
-                        if (file == "Metroid Planets v1.27g.exe")
-                        {
-                            switch (randomizerConfig.LevelData.Room)
-                            {
-                                case Room.rm_Zebeth:
-                                    File.Copy(Path.Combine(inputDir.FullName, file), Path.Combine(outputDir.FullName, "Planets_Zebeth.exe"), true);
-                                    break;
-                                case Room.rm_Novus:
-                                    File.Copy(Path.Combine(inputDir.FullName, file), Path.Combine(outputDir.FullName, "Planets_Novus.exe"), true);
-                                    break;
-                                default:
-                                    File.Copy(Path.Combine(inputDir.FullName, file), Path.Combine(outputDir.FullName, "Planets.exe"), true);
-                                    break;
-                        }
-                        }
-                        else
-                            File.Copy(Path.Combine(inputDir.FullName, file), Path.Combine(outputDir.FullName, file), true);
-                    }
             return 0;
         } catch (Exception e) {
             Console.WriteLine(e.Message);
